87e1fda7bf5afcc308ad925882a918a4e1046f02
Sindre Tupsjøen <sintup@hotmail.no>
2023-09-25 11:53:02 +0200

    Test

---
+++ Assets/Scripts/RollingBall.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] TriangleSurface triangleSurface;
+
+    vector3 StartPos;
+
+
+    float radius = 4f;
+    float velocity;
+    float x;
+    float y;
+    float z;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
